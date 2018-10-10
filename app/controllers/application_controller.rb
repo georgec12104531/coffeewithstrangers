839048d5f1d9bebdf7081957be2c1f663450d573
George Chang <georgecharleschang@gmail.com>
2018-10-10 12:11:40 -0700

    Add user model/session, application controller and sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,33 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def login
+    user.reset_session_token!
+    user.session_token = session[:session_token]
+    @current_user = user
+  end
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def require_login
+    if current_user.nil?
+      render json: { base: ['invalid credentials!'] }, status: 401 &&
+      redirect_to new_session_url
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+
 end
--- frontend/coffee_with_strangers.jsx
+++ frontend/coffee_with_strangers.jsx
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom';
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
   // const store = configureStore();
-  ReactDOM.render(<h1>Hey, welcome to CoffeeWithStrangers!</h1>, root);
+  ReactDOM.render(<h1>Hey, welcome to triple-grande-hotpumpkin-spicelatte-extra-hot-extra-whip-no-fome-stirred-two-pump-doublecup-with-three-ice-cubesWithStrangers!</h1>, root);
 });
