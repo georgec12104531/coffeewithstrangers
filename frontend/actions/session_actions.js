8946c7b984c0ff5ce4a2e7acd0dfa1dd80372e18
George Chang <georgecharleschang@gmail.com>
2018-10-10 17:15:26 -0700

    npm install

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -6,4 +6,12 @@
 // receiveErrors(errors) (regular action creator)
 
 
-export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER'
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+// export const signup = (user) => {
+//   return dispatch => {
+//     return Session
+//   }
+// }
