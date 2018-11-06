cc325ddd2a69c5d47b6d8f0106e204c2b301aded
George Chang <georgecharleschang@gmail.com>
2018-11-05 17:02:24 -0800

    Fix flexbox for description

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -12,54 +12,39 @@ class Splash extends React.Component {
           <div className="background-film">
             <div className="background-text-group">
               <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet" />
-              <div><h1 className="header-text">Everyone is interesting</h1></div>
-              <div>
-                <h2 className="header-2-text">
-                  ...but you don't discover that when you're staring a screen
-                </h2>
-              </div>           
-              <div>
-                <Link className="tea-time-button" to="/coffee-times">
-                  LET'S GET COFFEE
-                </Link>
-              </div>
+              <h1 className="header-text">Everyone is interesting</h1>
+              <h2 className="header-2-text">
+                ...but you don't discover that when you're staring a screen
+              </h2>
+              <Link className="tea-time-button" to="/coffee-times">
+                LET'S GET COFFEE
+              </Link>
             </div>
           </div>
         </div>
-
-        <div className="description-background">
+        <div className="description-container">
           <div className="description-main">
-            <h2 className="description-main-1">
-              {/* &ensp; &emsp; Show up for coffee */}
-              Show up for coffee
-            </h2>
-            <h2 className="descrition-main-2">
-              &ensp; &nbsp; Have a real conversation
-            </h2>
-            <h2 className="descrition-main-3">See what happens</h2>
-          </div>
-          <div className="description">
-            <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet" />
-            <h3 className="description-1">
+            <h2 className="description-header">Show up for coffee</h2>
+            <h3 className="description-message">
               You and a few others join a host at a cafe.
             </h3>
-            <h3 className="description-2">
-              {" "}
+          </div>
+          <div className="description-main">
+            <h2 className="description-header">Have a real conversation</h2>
+            <h3 className="description-message">
               You talk for two hours about anything.
             </h3>
-            <h3 className="description-3">
-              {" "}
+          </div>
+          <div className="description-main">
+            <h2 className="description-header">
+              See what happens
+            </h2>
+            <h3 className="description-message">
               That's it. No strings attached.
             </h3>
           </div>
-        </div>
-
-        <div className="test">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-        </div>
-      </div>;
+      </div>
+    </div>
   }
 
 }
