b6423fca64d00c14d11ab071de8f1b086b414fde
George Chang <georgecharleschang@gmail.com>
2018-11-17 17:04:50 -0800

    Render progress bar logic

--- frontend/components/coffee_times/progress_bar.jsx
+++ frontend/components/coffee_times/progress_bar.jsx
@@ -7,35 +7,28 @@ class ProgressBar extends React.Component {
 
 
   render() {
-    
-    // const closed_seats = this.props.coffeeTime.coffee_time_attendees.map((attendee, i) => {
-    //   return(
-    //     <div key={i} className="closed">"closed"</div>
-    //   )
-    // })
 
     const closed_seats = () => {
       let closed = [];
       this.props.coffeeTime.coffee_time_attendees.forEach((attendee) => {
-        <div >{closed.push("c")}</div>
+        closed.push(<div className="closed-bar"></div>)
       });
       return closed;
     }
-
     
     let open_seats_count = 5 - (Object.keys(this.props.coffeeTime.coffee_time_attendees).length)
 
     const open_seats = () => {
       let open = [];
       for (let i = 0; i < open_seats_count; i++) {
-        <div>{open.push("o")}</div>
+        open.push(<div className="open-bar"></div>)
       }
       return open;
     }
 
-    return <div>
-        <div>{closed_seats()}</div>
-        <div>{open_seats()}</div>
+    return <div className="progress-bar">
+        <div className="bar-container">{closed_seats()}</div>
+        <div className="bar-container">{open_seats()}</div>
       </div>;
     
     
