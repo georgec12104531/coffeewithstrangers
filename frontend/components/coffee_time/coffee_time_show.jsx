97a23195425fe92d3696fc65f384a5374e15ef3f
George Chang <georgecharleschang@gmail.com>
2018-11-29 19:55:22 -0800

    Render picture using background image url

--- frontend/components/coffee_time/coffee_time_show.jsx
+++ frontend/components/coffee_time/coffee_time_show.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import CoffeeTimeBox from './coffee_time_box';
 import coffeeone from './coffeeone.png';
-
+import profile from './profile.png';
+import asdf from './asdf.jpg';
+import image from './image.png';
 class CoffeeTime extends React.Component {
   constructor(props) {
     super(props);
@@ -24,8 +26,11 @@ class CoffeeTime extends React.Component {
             Sign me Up
           </button>
           <div className="coffee-time-side-bar-info-container">
-            <h5>WHAT IS COFFEE TIME EXACTLY?</h5>
-            <img src={coffeeone}/>
+            <h5 className="coffee-time-side-bar-info-intro">
+              WHAT IS COFFEE TIME EXACTLY?
+            </h5>
+            <img className="coffee-cup" src={coffeeone} />
+            <div className="ct-side-bar-info-line" />
             <p>
               Tea Time is where five-ish strangers sit at a cafe with a Host
               to have a two hour conversation. It's not about anything in
@@ -36,8 +41,19 @@ class CoffeeTime extends React.Component {
               come, aside from the fact that they all signed up for this —
               just like you. It's self-selecting in that sense.
             </p>
-            <p>However, it may bring you comfort to know a little something about your Host. If so, just keep reading</p>
+            <p>
+              However, it may bring you comfort to know a little something
+              about your Host. If so, just keep reading
+            </p>
           </div>
+        </div>
+        <div className="meet-your-host-container">
+          <div className="host-intro-container">
+            <div className="host-intro">Meet Your Host,{" "}{this.props.coffeeTime.host_name}</div>
+            <h5 className="host-intro-message">(It'll be helpful to know what they look like when you're looking for a group of confused strangers at the cafe.)</h5>
+          </div>
+          <div className="profile-pic"></div>
+          <div className="host-box-info-container"></div>
         </div>
       </div>;
   }
