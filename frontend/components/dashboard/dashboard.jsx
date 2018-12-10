8585bb09a56ee824b875647632f59ef60b8f2a4e
George Chang <georgecharleschang@gmail.com>
2018-12-09 21:46:04 -0800

    Render dashboard nav

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -3,10 +3,20 @@ import React from 'react';
 
 class Dashboard extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+  }
+
   render() {
-    return(
-      <div>We made it to the dashboard!</div>
-    )
+    return <div className="dashboard-main-container">
+        <div className="dashboard-nav-main-container">
+          <div className="dashboard-nav">
+            <div className="dashboard-nav-bar">Your Coffee Times</div>
+            <div className="dashboard-nav-bar">Profile</div>
+          </div>
+        </div>
+      </div>;
   }
 }
 
