2287c63dcfd771f4223b58d1a43a7fe4c4829be9
George Chang <georgecharleschang@gmail.com>
2018-12-17 16:55:48 -0800

    Fix handleShowCoffeeTime on hostedcoffeetimes

--- frontend/components/dashboard/my_host_coffee_time_item.jsx
+++ frontend/components/dashboard/my_host_coffee_time_item.jsx
@@ -1,29 +1,44 @@
 import React from 'react';
 import moment from "moment";
 
-const MyHostCoffeeTimeItem = (props) => {
+class MyHostCoffeeTimeItem extends React.Component {
 
-  console.log(props.coffeeTime.id)
-  return <div className="my-coffee-time-item-container">
-    <div className="my-ct-date">
-      <div className="my-ct-weekday">
-        {moment(props.coffeeTime.date).format("dddd")}
+  constructor(props) {
+    super(props)
+
+    this.handleShowCoffeeTime = this.handleShowCoffeeTime.bind(this);
+  }
+
+  handleShowCoffeeTime(coffeeTime) {
+    return e => {
+      e.preventDefault();
+      this.props.history.push(`/coffee-times/${coffeeTime.id}`);
+    };
+  }
+
+
+  render() {
+    return <div className="my-coffee-time-item-container">
+      <div className="my-ct-date" onClick={this.handleShowCoffeeTime(this.props.coffeeTime)}>
+        <div className="my-ct-weekday">
+          {moment(this.props.coffeeTime.date).format("dddd")}
+        </div>
+        <div className="my-ct-month-day">
+          {moment(this.props.coffeeTime.date).format("MMM D")}
+        </div>
+        <div className="my-ct-time">
+          {this.props.coffeeTime.start_time} - {this.props.coffeeTime.end_time}
+        </div>
+        <h4 className="my-ct-address">
+          {this.props.coffeeTime.address},{" "}
+          {this.props.coffeeTime.address2}
+        </h4>
       </div>
-      <div className="my-ct-month-day">
-        {moment(props.coffeeTime.date).format("MMM D")}
+      <div className="my-coffeeTimes-cancel-button" onClick={() => this.props.deleteCoffeeTime(this.props.coffeeTime.id)} >
+        CANCEL COFFEE TIME
       </div>
-      <div className="my-ct-time">
-        {props.coffeeTime.start_time} - {props.coffeeTime.end_time}
-      </div>
-      <h4 className="my-ct-address">
-        {props.coffeeTime.address},{" "}
-        {props.coffeeTime.address2}
-      </h4>
-    </div>
-    <div className="my-coffeeTimes-cancel-button" onClick={() => props.deleteCoffeeTime(props.coffeeTime.id)} >
-      CANCEL COFFEE TIME
-    </div>
-  </div>;
+    </div>;
+  }
 }
 
 export default MyHostCoffeeTimeItem;
